4240d6b31270ca7c4cb4567bb0f4dd5dbe2a8b97
xiaofenfen <shufen_zheng@163.com>
2016-06-28 15:37:54 +0800

    adjust format

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function startTime(){
+function startTime() {
   var today = new Date();
   var y = today.getFullYear();
   var o = today.getMonth() + 1;
@@ -6,54 +6,53 @@ function startTime(){
   var h = today.getHours();
   var m = today.getMinutes();
   var s = today.getSeconds();
-	// add a zero in front of numbers<10
+  // add a zero in front of numbers<10
   m = checkTime(m);
   s = checkTime(s);
-  document.getElementById('txt').innerHTML = "现在时间：" + y + "年" + o  + "月" + d + "日" + h + ":" + m + ":" + s ;
+  document.getElementById('txt').innerHTML = "现在时间：" + y + "年" + o  + "月" + d + "日" + h + ":" + m + ":" + s;
   t = setTimeout('startTime()',1000);	
+
   var deadline = new Date("00:00:00 10/1/2016");
   var duration = deadline.getTime() - today.getTime();
-  //计算出相差天数
+  // 计算出相差天数
   var dayDuration = Math.floor(duration / 1000 / 60 / 60 / 24);
-  //计算出小时数
-  var leave1 = duration % (1000 * 60 * 60 * 24);//计算天数后剩余的毫秒数
+  // 计算出小时数
+  var leave1 = duration % (1000 * 60 * 60 * 24);// 计算天数后剩余的毫秒数
   var hourDuration = Math.floor(leave1 / 1000 / 60 / 60);
-  //计算相差分钟数
-  var leave2 = leave1 % (1000 * 60 * 60)//计算小时数后剩余的毫秒数
+  // 计算相差分钟数
+  var leave2 = leave1 % (1000 * 60 * 60)// 计算小时数后剩余的毫秒数
   var minuteDuration = Math.floor(leave2 / 1000 / 60);
-  //计算相差秒数
-  var leave3 = leave2 % (1000 * 60);//计算分钟后剩余秒数
+  // 计算相差秒数
+  var leave3 = leave2 % (1000 * 60);// 计算分钟后剩余秒数
   var secondDuration = Math.floor(leave3 / 1000); 
-  //倒计时天数
+  // 倒计时天数
   var day = dayDuration;
-  document.getElementById("conDay").innerHTML = day;
-  //倒计时小时
+  document.getElementById("con-day").innerHTML = day;
+  // 倒计时小时
   var hour = hourDuration;
-  document.getElementById("conHour").innerHTML = hour;
-  //倒计时分钟
+  document.getElementById("con-hour").innerHTML = hour;
+  // 倒计时分钟
   var minute = minuteDuration;
-  document.getElementById("conMinute").innerHTML = minute;
-  //倒计时秒
+  document.getElementById("con-minute").innerHTML = minute;
+  // 倒计时秒
   var second = secondDuration;
   var secondTimer;
-  function changeSecond(){
+
+  function changeSecond() {
     if (second > -1) {
-    document.getElementById("conSecond").innerHTML = second;
-    secondTimer = setTimeout(function(){changeSecond();},1000);
-    }
-    else{
-          clearTimeout(secondTimer);
-    }
-  second--;
+      document.getElementById("con-second").innerHTML = second;
+      secondTimer = setTimeout(function() {changeSecond();},1000);
+      }else {
+        clearTimeout(secondTimer);
+      }
+    second--;
 	}
-  secondTimer = setTimeout(function(){changeSecond();},1000);
+  secondTimer = setTimeout(function() {changeSecond();},1000);
 }
-function checkTime(i){
+
+function checkTime(i) {
   if (i<10) {
     i = "0" + i;
   }
-return i;
+  return i;
 }
-
-
-
